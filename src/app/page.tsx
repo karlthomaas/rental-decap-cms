2086212cd0931a3a9c53c862907c86fe3b1f2219
Karl-Thomas <karl-thomas@zink.ee>
2024-04-27 10:02:13 +0300

    Update homepage layout and add Button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { getMarkup } from "@/libs/utils";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   const hero = getMarkup("src/content/homepage", "hero.md");
@@ -11,10 +12,10 @@ export default function Home() {
 
   return (
     <main>
-      <div>
-        <h1>{data.title}</h1>
-        <p>{data.subtitle}</p>
-        <button type="button">{data.buttonText}</button>
+      <div className="mt-10 flex flex-col items-center">
+        <h1 className="text-2xl">{data.title}</h1>
+        <p className="text-lg">{data.subtitle}</p>
+        <Button>{data.buttonText}</Button>
       </div>
     </main>
   );
